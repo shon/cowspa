a871ae0b75554f28eb73eb2ae001e74b384fc8bb
Shekhar <pythonic@gmail.com>
2011-01-29 21:52:50 +0530

    more css improvements, placeholder and build changes to support nested directories

--- fe/build/compile.py
+++ fe/build/compile.py
@@ -1,17 +1,29 @@
 import os
 from jinja2 import Environment, FileSystemLoader
 
-outdir = "out"
+outdir = "pub"
+srcdir = "fe/src"
+
 if not os.path.exists(outdir):
     os.mkdir(outdir)
-    cmd = "cp -rv fe/src/js " + outdir
-    os.system(cmd)
-    cmd = "cp -rv fe/src/css " + outdir
-    os.system(cmd)
+    for dirname in ('js', 'css', 'images'):
+        cmd = "cp -rv fe/src/%s %s" % (dirname, outdir)
+        os.system(cmd)
 
-name = "dashboard.html"
 env = Environment(loader=FileSystemLoader('fe/src'))
-print "compiling ", name
-template = env.get_template(name)
-out = template.render()
-file(os.path.join(outdir, name), 'w').write(out)
+for root, dirs, filenames in os.walk(srcdir):
+    for filename in filenames:
+        if not filename.endswith('.htm'): continue
+        print "compiling", os.path.join(root, filename)
+        reldir = root.replace(srcdir, '')
+        if reldir.startswith('/'):
+            reldir = reldir[1:]
+        relpath = os.path.join(reldir, filename)
+        template = env.get_template(relpath)
+        out = template.render()
+        dstdir = os.path.join(outdir, reldir)
+        if not os.path.exists(dstdir):
+            os.makedirs(dstdir)
+        outpath = os.path.join(dstdir, filename)#.replace('.htm', ''))
+        print "writing ", outpath
+        file(outpath, 'w').write(out)
