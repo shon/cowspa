e1383fe1d2c2d1c0e3b60f35c4870e645d5cf60a
Shekhar <pythonic@gmail.com>
2011-01-31 15:21:46 +0530

    path calculations for supporting relative paths

--- fe/build/compile.py
+++ fe/build/compile.py
@@ -11,19 +11,23 @@ if not os.path.exists(outdir):
         os.system(cmd)
 
 env = Environment(loader=FileSystemLoader('fe/src'))
+
 for root, dirs, filenames in os.walk(srcdir):
     for filename in filenames:
-        if not filename.endswith('.htm'): continue
+        if (not filename.endswith('.htm') and not 'extends' in file(os.path.join(root, filename)).read()): continue
         print "compiling", os.path.join(root, filename)
         reldir = root.replace(srcdir, '')
-        if reldir.startswith('/'):
+        if reldir.startswith(os.sep):
             reldir = reldir[1:]
+        rel_level_to_pubroot = len([x for x in reldir.split(os.path.sep) if x])
+        relpath = os.path.join(reldir, filename)
+        relpath_to_pubroot = os.path.join(*['..' for i in range(rel_level_to_pubroot)]) if rel_level_to_pubroot else '.'
         relpath = os.path.join(reldir, filename)
         template = env.get_template(relpath)
-        out = template.render()
+        out = template.render(pubroot=relpath_to_pubroot)
         dstdir = os.path.join(outdir, reldir)
         if not os.path.exists(dstdir):
             os.makedirs(dstdir)
-        outpath = os.path.join(dstdir, filename)#.replace('.htm', ''))
+        outpath = os.path.join(dstdir, filename.replace('.htm', ''))
         print "writing ", outpath
         file(outpath, 'w').write(out)
