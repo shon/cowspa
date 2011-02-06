24ad2455a9db08baad994a1c5ee7f2ef57f5d29b
Shekhar <pythonic@gmail.com>
2011-02-07 02:54:27 +0530

    flask serves page data (json), mustache loads data to page. footer added. and more

---
+++ be/run_webserver.py
@@ -0,0 +1,28 @@
+import os
+import time
+import testdata
+from flask import Flask, jsonify, url_for
+app = Flask(__name__)#, static_path=os.path.abspath("../pub"))
+static_dir = "../pub"
+
+@app.route('/app/<path:path>')
+def default(path):
+    time.sleep(2);
+    data = getattr(testdata, path, {'error':'no donuts for you'})
+    print data
+    return jsonify(data=data)
+
+@app.route('/<path:path>')
+def static(path):
+    print 'req for ', path
+    fspath = os.path.join(static_dir, path)
+    filename = os.path.basename(path)
+    if '.' in path:
+        content_type = "text/" + path.split('.')[-1]
+    else:
+        content_type = "text/html"
+    return file(fspath).read(), 200, {'Content-Type': content_type +'; charset=utf-8'}
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
---
+++ be/testdata.py
@@ -0,0 +1,7 @@
+dashboard = dict(
+    events = [
+        dict(start="915", end="1230", name="Ethical music", resource_name="GlassHouse", attending=13, link="/events/313", resource_link="/4/resources/12", bookedfor_name="Shammi Kapoor", bookedfor_url="/users/197", tags=['Today', 'GlassHouse', '@Shammi Kapoor']),
+        dict(start="1000", end="1300", name="Python Hackers meet", resource_name="White House", attending=7, link="/events/314", resource_link="/4/resources/10", bookedfor_name="BDFL", bookedfor_url="/users/190", tags=['Today', 'White House', '@Shammi Kapoor']),
+        dict(start="1700", end="2100", name="Wine tasting festival", resource_name="Terrace", attending=77, link="/events/315", resource_link="/4/resources/3", bookedfor_name="John T", bookedfor_url="/users/19", tags=['Today', 'Terrace', '@John T']),
+        ]
+)
